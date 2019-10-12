8171d74659d18a041f40df3fa6c4196e82489f4a
ibraheem <rules656@gmail.com>
2019-10-12 10:24:21 +0300

    added loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const gifts = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(gifts) {
+  let i = 0; 
+  while (i < gifts.length) {
+    console.log();
+    gifts[i]=`Thank you, ${gifts[i]}, for the wonderful surprise gift!`;
+    i++; 
+  }
+
+  return gifts;
+}
+
+writeCards(gifts);
+
+
+function countdown(number) {
+ for(let i =number;i>=0;i--){
+    console.log(number);  
+    number-=1;
+ }
+
+  }
